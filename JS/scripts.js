8d67d98c230eece886a29b9c52f495a5645fe640
DeveloperCooper <alexandrealvs01@gmail.com>
2023-05-15 19:08:46 -0300

    primeiro commit

---
+++ JS/scripts.js
@@ -0,0 +1,33 @@
+//TEC INFO
+
+const elements = {
+    date: document.querySelector(".date"),
+
+    scrollLinks: document.querySelectorAll(".navbar-list-link, .footer-link"),
+    navbarList: document.querySelector(".navbar-list"),
+    toggle: document.querySelector(".navbar-header-button"),
+}
+
+elements.date.innerHTML = new Date().getFullYear() + ".";
+
+elements.scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+        elements.navbarList.classList.remove("navbar-list--show-links");
+
+        const id = link.getAttribute("href");
+        const element = document.querySelector(id);
+
+        const position = element.offsetTop - 75;
+
+        window.scrollTo({
+            top: position,
+            behavior: "smooth"
+        });
+
+        e.preventDefault();
+    });
+});
+
+elements.toggle.addEventListener("click", e => {
+    elements.navbarList.classList.toggle("navbar-list--show-links");
+});
